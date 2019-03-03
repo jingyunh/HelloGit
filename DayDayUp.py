d5480cd224fb55ba3cbf8c7ee511bf770a8f3f88
Jingyun Huang <jingyun.huang@outlook.com>
2019-03-03 15:16:30 +0800

    ddu

---
+++ DayDayUp.py
@@ -0,0 +1,13 @@
+#DayDayUp.py
+def dayUp(df):
+    dayup=1
+    for i in range(365):
+        if i % 1 in [6,0]:
+            dayup=dayup*(1-df)
+        else:
+            dayup=dayup*(1+df)
+    return dayup
+dayfactor = 0.01
+while dayUp(dayfactor)<37.78:
+    dayfactor +=0.001
+print("工作日的努力参数是：{:.3f}".format(dayfactor))
